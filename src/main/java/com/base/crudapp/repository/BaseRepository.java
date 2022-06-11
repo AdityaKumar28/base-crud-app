fcac105d9257f9dfc6628e36687157644b42cfa9
Aditya Kumar <aditya.kumar@rara.delivery>
2022-06-11 22:37:13 +0530

    Added Paging and sorting to repository interface
    Signed-off-by: Aditya Kumar <aditya.kumar@rara.delivery>

--- src/main/java/com/base/crudapp/repository/BaseRepository.java
+++ src/main/java/com/base/crudapp/repository/BaseRepository.java
@@ -3,8 +3,9 @@ package com.base.crudapp.repository;
 import com.base.crudapp.entity.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.io.Serializable;
 
-public interface BaseRepository<T extends BaseEntity<ID>, ID extends Serializable> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
+public interface BaseRepository<T extends BaseEntity<ID>, ID extends Serializable> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T>, PagingAndSortingRepository<T, ID> {
 }
